81b6415d7781559bcde0c7b9bd9fd73bf3925702
Andre Azevedo <AndreAzevedoCoder@gmail.com>
2020-03-16 00:10:34 -0300

    tentando docker

--- BD/scripts/DDL.sql
+++ BD/scripts/DDL.sql
@@ -1,12 +1,16 @@
 CREATE DATABASE sistemaDePontos;
+GO
+
 USE sistemaDePontos
+GO
 
 CREATE TABLE funcionarios (
 	idFuncionario INT PRIMARY KEY IDENTITY,
-	nome TEXT,
-	nickname TEXT,
-	senha TEXT,
+	nome NVARCHAR(255),
+	email NVARCHAR(255),
+	senha NVARCHAR(255),
 );
+GO
 
 CREATE TABLE diasDeTrabalho (
 	idDia INT PRIMARY KEY IDENTITY,
@@ -16,4 +20,13 @@ CREATE TABLE diasDeTrabalho (
 	intervaloEntrada DATETIME2,
 	intervaloSaida DATETIME2,
 	saida DATETIME2,
+);
+GO
+
+CREATE TABLE historico (
+	idHistorico INT PRIMARY KEY IDENTITY,
+	idFuncionario INT FOREIGN KEY REFERENCES funcionarios(idFuncionario),
+	idDia INT,
+	ocorrido NVARCHAR(255) NOT NULL,
+	quando DATETIME2 NOT NULL,
 );
